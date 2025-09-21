24de786c006e8b21b4cd103d4555129ea0ec4889
ndishimye emilien <ndishimyeemilien29@gmail.com>
2025-09-21 19:21:58 +0000

    use this fire base{// Import the functions you need from the SDKs you ne

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,4 +1,4 @@
-import { initializeApp, getApps, App, cert, getApp } from "firebase-admin/app";
+import { initializeApp, getApps, App, getApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { credential } from "firebase-admin";
 
@@ -7,13 +7,13 @@ let app: App;
 if (!getApps().length) {
   app = initializeApp({
     credential: credential.applicationDefault(),
-    databaseURL: `https://code-ai-fcd40.firebaseio.com`,
+    databaseURL: "https://code-ai-fcd40.firebaseio.com",
     projectId: "code-ai-fcd40",
-    storageBucket: `code-ai-fcd40.appspot.com`,
-    authDomain: `code-ai-fcd40.firebaseapp.com`,
+    storageBucket: "code-ai-fcd40.appspot.com",
+    authDomain: "code-ai-fcd40.firebaseapp.com",
   });
 } else {
-  app = getApps()[0];
+  app = getApp();
 }
 
 const auth = getAuth(app);
